ab17e352e3ea6ecfb48d0316ae334bd26ccdf095
Brian Chung <brianchung@live.com>
2020-06-10 13:55:50 -0500

    adds Dark Sky API

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,22 @@
 const functions = require('firebase-functions');
+const axios = require('axios');
+const cors = require('cors')({ origin: true });
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+const url = `https://api.forecast.io/forecast/${functions.config().darksky.key}/`;
+
+exports.darksky = functions.https.onRequest((req, res) => {
+  if (req.method !== 'GET') {
+    return res.status(403).send('Forbidden!');
+  }
+
+  return cors(req, res, () => {
+    return axios
+      .get(`${url}${req.query.lat},${req.query.lng}?exclude=minutely`)
+      .then(response => res.send(response.data))
+      .catch(err =>
+        res.status(500).json({
+          error: err,
+        })
+      );
+  });
+});
