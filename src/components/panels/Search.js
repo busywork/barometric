54118b6e06e89f8a153cb755bbd54dc281759cdb
Brian Chung <brianchung@live.com>
2020-06-22 16:44:28 -0500

    adds geolocation

--- src/components/panels/Search.js
+++ src/components/panels/Search.js
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
-import { Col } from 'react-bootstrap';
+import { Alert, Col } from 'react-bootstrap';
 import styled from 'styled-components';
 import Geosuggest from 'react-geosuggest';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
@@ -10,17 +10,40 @@ import { fetchWeather } from '../../redux/weather';
 
 const Search = () => {
   const dispatch = useDispatch();
+  const [error, setError] = useState(undefined);
 
   const onSuggestSelect = e => {
+    setError(undefined);
     if (e) dispatch(fetchWeather(e.location));
+  };
+
+  const getPosition = e => {
+    setError(undefined);
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        ({ coords }) => {
+          dispatch(fetchWeather({ lat: coords.latitude, lng: coords.longitude }));
+        },
+        err => {
+          setError(err.message);
+        },
+        { enableHighAccuracy: true }
+      );
+    }
   };
 
   return (
     <Col>
       <SearchBar>
         <Geosuggest onSuggestSelect={onSuggestSelect} className="mr-auto" />
-        <FontAwesomeIcon className="text-white my-4 mx-4" icon={faMapMarkerAlt} size="2x" />
+        <FontAwesomeIcon
+          onClick={getPosition}
+          className="text-white my-4 mx-4"
+          icon={faMapMarkerAlt}
+          size="2x"
+        />
       </SearchBar>
+      {error && <Alert variant="warning">{error}</Alert>}
     </Col>
   );
 };
